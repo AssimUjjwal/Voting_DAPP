e29248885cd5cc38777cacdbfe109f0b0513a383
Assim Ujjwal <assim.ujjwal.civ20@itbhu.ac.in>
2023-03-13 12:53:52 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './history';
+import Homepage from './components/Homepage';
+import AddCandidate from './components/AddCandidate';
+import Candidates from './components/Candidates';
+import VerifyVoter from './components/VerifyVoter';
+import Results from './components/Results';
+import Phase from './components/Phase';
+import Voting from './components/Voting';
+import Register from './components/Register'
+
+function App() {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/AddCandidate' component={AddCandidate} />
+          <Route path='/CandidateDetails' component={Candidates} />
+          <Route path='/VerifyVoter' component={VerifyVoter} />
+          <Route path='/Result' component={Results} />
+          <Route path='/Phase' component={Phase} />
+          <Route path='/Vote' component={Voting} />
+          <Route path='/Register' component={Register} />
+        </Switch>
+      </Router>
+    );
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
